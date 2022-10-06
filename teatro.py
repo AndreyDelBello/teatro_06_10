5d1a9fc2b629f43e4d666b8358d68edefa3ba2e7
Andrey <andrey.delbello2002@gmail.com>
2022-10-06 14:18:52 -0300

    Teatro

---
+++ teatro.py
@@ -0,0 +1,11 @@
+linhas = 20
+colunas = 20
+
+matriz = []
+
+matriz[linha][coluna]
+
+for x in range(linhas):
+    linhas_matriz = []
+    for y in range(colunas):
+        linhas_matriz.append("d")
